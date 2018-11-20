aeb081a32a4716e479de34f55d87c579557d4c7d
Daniel Scholl <daniel.scholl@microsoft.com>
2018-11-20 09:30:30 -0600

    Added Sample Env file

---
+++ .env_sample.ps1
@@ -0,0 +1,9 @@
+$Env:AZURE_TENANT = "<tenant_id>"
+$Env:AZURE_SUBSCRIPTION = "<sub_id>"
+$Env:AZURE_PRINCIPAL = "<app_id>"
+
+$Env:AZURE_RANDOM = "123"
+$Env:AZURE_GROUP = "fabric"
+$Env:AZURE_LOCATION = "eastus2"
+$Env:AZURE_USERNAME = "<username>"
+$Env:AZURE_PASSWORD = "<password>"
